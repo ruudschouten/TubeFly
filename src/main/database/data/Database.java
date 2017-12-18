95f3fa34f41baccb5b9bdea28e7dac8145e1f003
Ruud Schouten <ruud.schouten@outlook.com>
2017-12-18 14:59:35 +0100

    Updated database

--- src/main/database/data/Database.java
+++ src/main/database/data/Database.java
@@ -1,15 +1,33 @@
 package main.database.data;
 
+import main.log.Logger;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.logging.Level;
 
 public class Database {
+    private Logger logger;
     private Properties props;
     private Connection con;
+
+    public Database() {
+        logger = new Logger("Database", Level.SEVERE, Level.SEVERE);
+        try {
+            setupProperty();
+            initConnection();
+        } catch (IOException | SQLException e) {
+            logger.log(Level.SEVERE, e.toString());
+        }
+    }
+
+    public Connection getCon() {
+        return con;
+    }
 
     private void setupProperty() throws IOException {
         props = new Properties();
