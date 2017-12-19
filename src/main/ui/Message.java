70d26defb734f788fdb512e46e3a289174bfb10c
Ruud Schouten <ruud.schouten@outlook.com>
2017-12-19 09:39:14 +0100

    Added MessageBox

---
+++ src/main/ui/Message.java
@@ -0,0 +1,12 @@
+package main.ui;
+
+import javafx.scene.control.Alert;
+
+public class Message {
+    public static void Show(String title, String body) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(title);
+        alert.setContentText(body);
+        alert.show();
+    }
+}
