7e886beff797930032ae4c8939269a43efcbbccf
Ruud Schouten <ruud.schouten@outlook.com>
2017-12-19 09:38:59 +0100

    Added log, now checks if database is offline

--- src/main/ui/client/controllers/LoginController.java
+++ src/main/ui/client/controllers/LoginController.java
@@ -1,38 +1,64 @@
 package main.ui.client.controllers;
 
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import main.database.data.Database;
+import main.database.logic.UserContext;
+import main.database.repositories.UserRepository;
+import main.log.Logger;
+import main.play.User;
+import main.ui.Message;
 import main.ui.UITools;
 
+import java.sql.SQLException;
+import java.util.logging.Level;
+
 public class LoginController {
+    @FXML private TextArea taLog;
     @FXML private TextField tbMail;
     @FXML private PasswordField tbPassword;
 
     private Stage stage;
+    private Logger logger;
+
+    public void initialize() {
+        logger = new Logger("LoginController", Level.SEVERE, Level.SEVERE);
+        if(Database.getCon() != null) {
+            taLog.setText(taLog.getText() + "[INFO] Server is online\n");
+        } else {
+            taLog.setText(taLog.getText() + "[ERROR] Server is offline\n");
+        }
+    }
 
     public void setStage(Stage stage) {
         this.stage = stage;
     }
 
     public void showMain(ActionEvent actionEvent) {
-        //TODO: Check login details
-
-        UITools.UIManager uiManager = new UITools.UIManager();
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                uiManager.loadFXML("main/resources/playlistcollection.fxml", "Playlists");
+        UserRepository userRepository = new UserRepository(new UserContext());
+        String mail = tbMail.getText();
+        String pass = tbPassword.getText();
+        try {
+            User user = userRepository.login(mail, pass);
+            if(user != null) {
+                UITools.UIManager uiManager = new UITools.UIManager();
+                uiManager.loadFXML("/main/resources/playlistcollection.fxml", "Playlists");
+            } else {
+                Message.Show("Invalid login", "Username and password did not match\nPlease try again.");
+                logger.log(Level.WARNING, "Username and password did not match");
             }
-        });
+        } catch (SQLException e) {
+            logger.log(Level.SEVERE, e.toString());
+        }
     }
 
     public void showRegUser(MouseEvent mouseEvent) {
         UITools.UIManager uiManager = new UITools.UIManager();
-        uiManager.loadFXML("main/resources/register.fxml", "Playlists");
+        uiManager.loadFXML("/main/resources/register.fxml", "Playlists");
     }
 }
