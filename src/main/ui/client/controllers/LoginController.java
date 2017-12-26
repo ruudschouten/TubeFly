3a13149024fbf99d5c04f817cb3bec989981f4ff
Ruud Schouten <zzding@live.nl>
2017-12-26 20:23:20 +0100

    Now gets user from Container

--- src/main/ui/client/controllers/LoginController.java
+++ src/main/ui/client/controllers/LoginController.java
@@ -6,16 +6,14 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
-import javafx.stage.Stage;
 import database.data.Database;
-import database.logic.UserContext;
-import database.repositories.UserRepository;
 import log.Logger;
 import play.User;
+import rmi.ClientContainer;
 import ui.Message;
+import ui.ResourceHandler;
 import ui.UITools;
 
-import java.sql.SQLException;
 import java.util.logging.Level;
 
 public class LoginController {
@@ -23,42 +21,35 @@ public class LoginController {
     @FXML private TextField tbMail;
     @FXML private PasswordField tbPassword;
 
-    private Stage stage;
     private Logger logger;
+    private UITools.UIManager uiManager;
 
     public void initialize() {
         logger = new Logger("LoginController", Level.SEVERE, Level.SEVERE);
-        if(Database.getCon() != null) {
+        if (Database.getCon() != null) {
             taLog.setText(taLog.getText() + "[INFO] Server is online\n");
         } else {
             taLog.setText(taLog.getText() + "[ERROR] Server is offline\n");
         }
-    }
 
-    public void setStage(Stage stage) {
-        this.stage = stage;
+        uiManager = new UITools.UIManager();
     }
 
     public void showMain(ActionEvent actionEvent) {
-        UserRepository userRepository = new UserRepository(new UserContext());
         String mail = tbMail.getText();
         String pass = tbPassword.getText();
-        try {
-            User user = userRepository.login(mail, pass);
-            if(user != null) {
-                UITools.UIManager uiManager = new UITools.UIManager();
-                uiManager.loadFXML("menu.fxml", "Menu");
-            } else {
-                Message.show("Invalid login", "Username and password did not match\nPlease try again.");
-                logger.log(Level.WARNING, "Username and password did not match");
-            }
-        } catch (SQLException e) {
-            logger.log(Level.SEVERE, e.toString());
+        ClientContainer container = ResourceHandler.getContainer();
+        User user = container.login(mail, pass);
+        if (user != null) {
+            UITools.UIManager uiManager = new UITools.UIManager();
+            uiManager.loadFXML("menu.fxml", "Menu");
+        } else {
+            Message.show("Invalid login", "Username and password did not match\nPlease try again.");
+            logger.log(Level.WARNING, "Username and password did not match");
         }
     }
 
     public void showRegUser(MouseEvent mouseEvent) {
-        UITools.UIManager uiManager = new UITools.UIManager();
         uiManager.loadFXML("register.fxml", "Register");
     }
 }
