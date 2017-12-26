c39467b7ae966afaff2f895f57d4d5aacf06286a
Ruud Schouten <zzding@live.nl>
2017-12-26 20:34:52 +0100

    Removed unneeded class

--- src/main/ui/client/RMIClient.java
+++ src/main/ui/client/RMIClient.java
@@ -4,6 +4,8 @@ import fontyspublisher.IRemotePublisherForListener;
 import fontyspublisher.SharedData;
 import log.Logger;
 import rmi.ClientContainer;
+import rmi.IContainer;
+import ui.ResourceHandler;
 
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
@@ -11,37 +13,40 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.logging.Level;
 
-public class RMIClient {
-    private int port;
-    private String registryName;
-    private String serverName;
-    private String publisherName;
-
+class RmiClient {
     private Logger logger;
+    private ClientContainer container;
+
+    //RMI
+    private static int port;
+    private static String registryName;
+    private static String serverName;
+    private static String publisherName;
+
+    RmiClient(String loggerName) {
+        logger = new Logger(loggerName, Level.ALL, Level.SEVERE);
+        initSharedData();
+        initPublisher();
+
+        ResourceHandler.setContainer(container);
+        ResourceHandler.setLogger(logger);
+    }
 
     private void initSharedData() {
-        //Zodat deze bij server kan
         port = SharedData.getPort();
         registryName = SharedData.getRegistryName();
         serverName = SharedData.getServerName();
         publisherName = SharedData.getPublisherName();
     }
 
-    public void setupManager() {
-        logger = new Logger("RmiClient", Level.ALL, Level.SEVERE);
-        initSharedData();
-        ClientContainer container;
-
-        //Maakt ClientManager aan
+    private void initPublisher() {
         try {
             Registry registry = LocateRegistry.getRegistry(registryName, port);
-            //TODO: Set all interfaces
             IRemotePublisherForListener publisher = (IRemotePublisherForListener) registry.lookup(publisherName);
-//            IPartyManager server = (IPartyManager) registry.lookup(serverName);
-//            container = new ClientContainer(publisher, server);
-
+            IContainer server = (IContainer) registry.lookup(serverName);
+            container = new ClientContainer(publisher, server);
         } catch (RemoteException | NotBoundException e) {
-            logger.log(Level.INFO, e.getMessage());
+            logger.log(Level.WARNING, e.getMessage());
         }
     }
 }
