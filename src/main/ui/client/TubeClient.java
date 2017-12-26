404a5a94dfa671d9a74bcd97e684085ae2438bc7
Ruud Schouten <zzding@live.nl>
2017-12-26 20:22:39 +0100

    Client now uses RMI

--- src/main/ui/client/TubeClient.java
+++ src/main/ui/client/TubeClient.java
@@ -1,19 +1,59 @@
 package ui.client;
 
+import fontyspublisher.IRemotePublisherForListener;
+import fontyspublisher.SharedData;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import log.Logger;
 import rmi.ClientContainer;
+import rmi.IContainer;
+import ui.ResourceHandler;
 import ui.UITools;
 
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+
 public class TubeClient extends Application {
-    ClientContainer container;
+    private Logger logger;
+    private ClientContainer container;
+
+    //RMI
+    private static int port;
+    private static String registryName;
+    private static String serverName;
+    private static String publisherName;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        logger = new Logger("TubeClient", Level.ALL, Level.SEVERE);
+        initSharedData();
+        initPublisher();
         primaryStage.close();
-//        container = new ClientContainer();
+
+        ResourceHandler.setContainer(container);
+        ResourceHandler.setLogger(logger);
         UITools.UIManager uiManager = new UITools.UIManager();
-        uiManager.setContainer(container);
         uiManager.loadFXML("login.fxml", "Login");
+    }
+
+    private void initSharedData() {
+        port = SharedData.getPort();
+        registryName = SharedData.getRegistryName();
+        serverName = SharedData.getServerName();
+        publisherName = SharedData.getPublisherName();
+    }
+
+    private void initPublisher() {
+        try {
+            Registry registry = LocateRegistry.getRegistry(registryName, port);
+            IRemotePublisherForListener publisher = (IRemotePublisherForListener) registry.lookup(publisherName);
+            IContainer server = (IContainer) registry.lookup(serverName);
+            container = new ClientContainer(publisher, server);
+        } catch (RemoteException | NotBoundException e) {
+            logger.log(Level.WARNING, e.getMessage());
+        }
     }
 }
