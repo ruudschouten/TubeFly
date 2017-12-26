7f1be7f1086f1997f5f8f08ea97e2040f3de0d4c
Ruud Schouten <zzding@live.nl>
2017-12-26 20:18:16 +0100

    Updated with USER_PROPERTY

--- src/main/rmi/Properties.java
+++ src/main/rmi/Properties.java
@@ -1,6 +1,7 @@
 package rmi;
 
-public class Properties {
+final class Properties {
+    public final static String USER_PROPERTY = "user";
     public final static String ARTIST_PROPERTY = "artist";
     public final static String PLAYLIST_PROPERTY = "playlist";
 }
