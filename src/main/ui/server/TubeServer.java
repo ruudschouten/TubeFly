e76c10dcca8164b18fa6d325455496ab4d2fce86
Ruud Schouten <zzding@live.nl>
2017-12-26 20:22:03 +0100

    Server now uses RMI

--- src/main/ui/server/TubeServer.java
+++ src/main/ui/server/TubeServer.java
@@ -1,13 +1,56 @@
 package ui.server;
 
+import fontyspublisher.RemotePublisher;
+import fontyspublisher.SharedData;
+import log.Logger;
 import rmi.ServerContainer;
 
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.logging.Level;
+
 public class TubeServer {
-    ServerContainer container;
+    private static int port;
+    private static String serverName;
+    private static String publisherName;
+
+    private static Logger logger;
+    private static boolean keepRunning = true;
+    private static ServerContainer container;
 
     public static void main(String[] args) {
-        System.out.println("Server started");
-        //Do a thing
-        System.out.println("Server shut down");
+        logger = new Logger("TubeServer", Level.ALL, Level.ALL);
+        initSharedData();
+        if(!initRegistry()) return;
+        while(keepRunning) {
+
+        }
+    }
+
+    private static void initSharedData() {
+        port = SharedData.getPort();
+        serverName = SharedData.getServerName();
+        publisherName = SharedData.getPublisherName();
+        System.setProperty("java.rmi.server.hostname", SharedData.getRegistryName());
+    }
+
+    private static boolean initRegistry() {
+        try {
+            logger.log(Level.FINE, "Server will start.");
+            RemotePublisher publisher = new RemotePublisher();
+            Registry registry = LocateRegistry.createRegistry(port);
+            logger.log(Level.INFO, "Registry created");
+            registry.rebind(publisherName, publisher);
+            logger.log(Level.INFO, "Publisher bound to registry");
+            container = new ServerContainer(publisher);
+            registry.rebind(serverName, container);
+            logger.log(Level.INFO, "Server name bound to registry");
+            return true;
+        } catch (RemoteException e) {
+            logger.log(Level.WARNING, e.getMessage());
+            e.printStackTrace();
+            return false;
+        }
     }
 }
