3e6dfec32630108fa0e72c04f47dd68391b5af4d
Ruud Schouten <zzding@live.nl>
2018-01-11 20:48:49 +0100

    Removed Properties

--- src/main/rmi/Properties.java
+++
@@ -1,7 +0,0 @@
-package rmi;
-
-final class Properties {
-    public final static String USER_PROPERTY = "user";
-    public final static String ARTIST_PROPERTY = "artist";
-    public final static String PLAYLIST_PROPERTY = "playlist";
-}
